be6ae996d7713e84e61814b03b7ac37c4c6eca4a
Andrew Lingenfelter <luridsorcerer@gmail.com>
2025-09-28 20:29:35 -0400

    Scaled up 4x, disabled smoothing

--- demo.js
+++ demo.js
@@ -2,23 +2,24 @@ import { ffwindow } from "./ff-window.js";
 
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
+ctx.imageSmoothingEnabled=false;
+ctx.scale(4,4); 
 let i = new Image();
 
 function init() {
-
+    document.getElementById("msg").addEventListener("change",update);
 }
 
 function update() {
-    
+    msg = document.getElementById("msg").value;
+    i = ffwindow(msg);
 }
 
 function render() {
     // clear canvas
     ctx.fillStyle = "gray";
     ctx.fillRect(0,0,800,600);
-    // draw image
-    i = ffwindow("Thtring!? I guess so. $&%");
-    ctx.drawImage(i, 100, 100);
+    ctx.drawImage(i, 10, 10);
 }
 
 function run() {
